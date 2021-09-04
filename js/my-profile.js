a460995f43bb22bdd4c815b3e28fef50d8955a82
neysffer <jeniffer_s.f@hotmail.com>
2021-09-03 22:20:57 -0300

    Prueba Filtros
    Entrega 2

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,13 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+function mostrarUser(){
+    const user = document.getElementById("Usuario");
+    const u_ser = sessionStorage.getItem("Base_DATOS");
+    user.innerHTML = u_ser;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    mostrarUser();
 });
