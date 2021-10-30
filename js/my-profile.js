12b856208115bbca44b7d682cf14b33b66126d72
neysffer <jeniffer_s.f@hotmail.com>
2021-10-30 13:48:36 -0300

    Mejorando el perfil
    Cálculo de edad, etc...

--- js/my-profile.js
+++ js/my-profile.js
@@ -7,19 +7,36 @@ let controlImg;
 console.log(myDataBase);
 
 document.addEventListener("DOMContentLoaded", function (e) {
-  
-    document.getElementById("changeUserName").innerHTML = (myDataBase)[0].name;
-    /* document.getElementById("changeEmail").innerHTML = (myDataBase)[0].email; */
-    document.getElementById("changePlaceHolderEmail").placeholder = (myDataBase)[0].email;
+  //Recorro mi almacenamiento en el local y agrego la información dependiendo de si es login o registro
+  for(let i = 0; i < parseInt((myDataBase).length) ; i++)
+  {
+    
+    document.getElementById("changeIMG").innerHTML = ` <img class="rounded-circle mt-5" width="150px" src="` + myDataBase[i].img +`">`
+    
+    if((myDataBase[i].email == undefined) && (myDataBase[i].phone == undefined))
+    {
+      document.getElementById("changeUserName").innerHTML = (myDataBase)[i].name;
+      document.getElementById("changeName").innerHTML = (myDataBase)[i].user + " " + (myDataBase)[i].lastNam;
+      document.getElementById("age").placeholder = (myDataBase)[i].age;
 
-    document.getElementById("changePhone").placeholder = (myDataBase)[0].numbSelect + " " + (myDataBase)[0].phone;
+    }else{
+        document.getElementById("changeUserName").innerHTML = (myDataBase)[i].name;
+        /* document.getElementById("changeEmail").innerHTML = (myDataBase)[i].email; */
+        document.getElementById("changePlaceHolderEmail").placeholder = (myDataBase)[i].email;
 
-    document.getElementById("changeName").innerHTML = (myDataBase)[0].user + " " + (myDataBase)[0].lastNam;
+        document.getElementById("changeName").innerHTML = (myDataBase)[i].user + " " + (myDataBase)[i].lastNam;
 
-    document.getElementById("changeIMG").innerHTML = ` <img class="rounded-circle mt-5" width="150px" src="` + myDataBase[0].img +`">`
+        document.getElementById("changePhone").placeholder = (myDataBase)[i].phone;
+        document.getElementById("selectNumber").options.selectedIndex = myDataBase[i].numbSelect;
+
+        document.getElementById("age").placeholder = (myDataBase)[i].age;
+
+      }
+  }
 
     document.getElementById("btnSaveChange").addEventListener("click", changeInfo);
 
+/* if() */
     document.getElementById("imgModal").addEventListener("click", function(){
       controlImg = 0;
       changeImage(controlImg);
@@ -36,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
       controlImg = 3;
       changeImage(controlImg);
     });
+    
 });
 
 
@@ -44,28 +62,47 @@ function changeInfo()
     const arr = [];
     let newName = document.getElementById("nameUser").value;
     let newLastname = document.getElementById("lastName").value;
+    let newEmail = document.getElementById("changePlaceHolderEmail").value;
+    let newPhone = document.getElementById("changePhone").value;
+    let selected = document.getElementById("selectNumber");
+    let selectDate = document.getElementById("dp1").value;
+    let fecha1 = new Date(selectDate)
+    let fecha2 = new Date();
+    let resta = fecha2.getTime() - fecha1.getTime();
+    let age = (Math.round(resta/ (1000*60*60*24*365)-1));
+    /* let selectedNumberPhone = selected.options[selected.selectedIndex].text; */
     
+    if(newName !== "" && newLastname !== "" && newEmail !== "" && newPhone !== "")
+    {
+      document.getElementById("changeName").innerHTML = newName + " " + newLastname;
+      document.getElementById("age").placeholder = age;
+      arr.push({
+          "name" : myDataBase[0].name,
+          "email" : newEmail,
+          "phone" : newPhone,
+          "numbSelect" : selected.selectedIndex,
+          "pass" :  myDataBase[0].pass,
+          "user" : newName,
+          "lastNam": newLastname,
+          "img" : myDataBase[0].img,
+          "age" : age,
+      });
 
-    document.getElementById("changeName").innerHTML = newName + " " + newLastname;
+      
+      localStorage.setItem("Base_DATOS", JSON.stringify(arr));
+    }else{
+      alert("Llena todos los campos para continuar.")
+    }
 
-    arr.push({
-        "name" : myDataBase[0].name,
-        "email" : myDataBase[0].email,
-        "phone" : myDataBase[0].phone,
-        "numbSelect" : myDataBase[0].numbSelect,
-        "pass" :  myDataBase[0].pass,
-        "user" : newName,
-        "lastNam": newLastname,
-        "img" : myDataBase[0].img,
-    });
-
-  /* console.log(arr); */
-   localStorage.setItem("Base_DATOS", JSON.stringify(arr));
+    /* 
+    console.log(selected.selectedIndex); */
+   
 
 }
 
 
-function changeImage(directionImg){
+function changeImage(directionImg)
+{
   let arre = [];
   if(directionImg == 0){
     myDataBase[0].img = "/img/woman.png";
@@ -91,6 +128,18 @@ function changeImage(directionImg){
 });
   localStorage.setItem("Base_DATOS", JSON.stringify(arre));
 }
+
+$(document).ready(function(){
+
+  $('.datepicker').datepicker({
+  format: 'dd-mm-yyyy',
+  todayHighlight: true,
+  toggleActive: true
+  });
+  
+});
+
+
 
 /* para después */
  /*  function mostrar(){
